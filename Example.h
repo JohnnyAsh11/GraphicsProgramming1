52f86ccbdda736725991da50f95d4b7346f34506
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2025-04-17 10:46:50 -0400

    Pushing out some post processing psuedocode.

--- Example.h
+++ Example.h
@@ -7,3 +7,23 @@
 #endif
 
 // RenderDoc -> Shader debugging for everything.
+
+
+/*
+post processing pixel shader motion blur psuedocode.
+
+pixelWidth = 1.0f / windowWidth
+pixelHeight = 1.0f / windowHeight
+
+int sampleCount
+float3 totalColor = 0.0f;
+
+for (int -blur; int < blur; int++)
+	for (int -blur; int < blur; int++)
+		float2 uv = input.uv + float2(pixelWidth * x, pixelHeight * y);
+		totalColor += Sample(uv);
+		sampleCount++;
+
+return float4(totalColor / sampleCount, 0.0f);
+
+*/
